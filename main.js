c9e5cbf4417eb410ed55e2c71c30c3096f8d3f67
berma <adamscberman@gmail.com>
2013-12-01 17:03:53 -0600

    better

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Cache selectors
-var topMenu = $("#top-menu"),
+var lastId,
+    topMenu = $("#top-menu"),
     topMenuHeight = topMenu.outerHeight()+15,
     // All list items
     menuItems = topMenu.find("a"),
@@ -9,11 +10,22 @@ var topMenu = $("#top-menu"),
       if (item.length) { return item; }
     });
 
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 3000);
+  e.preventDefault();
+});
+
 // Bind to scroll
 $(window).scroll(function(){
    // Get container scroll position
    var fromTop = $(this).scrollTop()+topMenuHeight;
-
+   
    // Get id of current scroll item
    var cur = scrollItems.map(function(){
      if ($(this).offset().top < fromTop)
@@ -22,8 +34,12 @@ $(window).scroll(function(){
    // Get the id of the current element
    cur = cur[cur.length-1];
    var id = cur && cur.length ? cur[0].id : "";
-   // Set/remove active class
-   menuItems
-     .parent().removeClass("active")
-     .end().filter("[href=#"+id+"]").parent().addClass("active");
-});​
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
